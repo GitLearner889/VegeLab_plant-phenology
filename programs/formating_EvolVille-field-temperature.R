eb0987ef2095c954e324406e405b64886c3cb035
Gabriel Sagot <gabriel.sagot@proton.me>
2026-09-16 14:27:42 +0200

    cleaning and formating of location data

--- programs/formating_EvolVille-field-temperature.R
+++ programs/formating_EvolVille-field-temperature.R
@@ -1,4 +1,4 @@
-source(here::here("programs","libraries.R"))
+#source(here::here("programs","libraries.R"))
 
 requires(dplyr)
 requires(photobiology)
@@ -9,4 +9,16 @@ requires(photobiology)
 df_temperature_all_raw = read.table(here("data/data_environnement/ibutton_all.txt"), header = T, sep = "\t")
 # site location data retrival
 df_sites_info = read_excel(here("data/site.xlsx"))
+
+#### 1. Data cleaning ####
+
+##### 1.1 Location data #####
+# keep only location of iButton (inside trapnest)
+df_sites_positions = df_sites_info |> 
+  select(site_id,site_lon_trapnest,site_lat_trapnest) |> 
+  mutate(site_id = as.numeric(site_id),
+         site_lon_trapnest = as.numeric(site_lon_trapnest),
+         site_lat_trapnest = as.numeric(site_lat_trapnest))
+
+
 
