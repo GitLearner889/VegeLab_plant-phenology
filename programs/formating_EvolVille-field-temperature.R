2084a3adf8a93b74cbcc3a1edda82c0aec2b9013
Gabriel Sagot <gabriel.sagot@proton.me>
2026-09-16 14:40:11 +0200

    add packages

--- programs/formating_EvolVille-field-temperature.R
+++ programs/formating_EvolVille-field-temperature.R
@@ -1,13 +1,14 @@
 #source(here::here("programs","libraries.R"))
 
-
+require(here)
+require(readxl)
 require(dplyr)
 require(photobiology)
 
 #### 0. Data importation ####
 
 # iButton data retrival 
-df_temperature_all_raw = read.table(here("data/data_environnement/ibutton_all.txt"), header = T, sep = "\t")
+df_temperature_all_raw = read.table(here("data/data_environment/ibutton_all.txt"), header = T, sep = "\t")
 # site location data retrival
 df_sites_info = read_excel(here("data/site.xlsx"))
 
@@ -16,7 +17,7 @@ df_sites_info = read_excel(here("data/site.xlsx"))
 ##### 1.1 Location data #####
 # keep only location of iButton (inside trapnest)
 df_sites_positions = df_sites_info |> 
-  select(site_id,site_lon_trapnest,site_lat_trapnest) |> 
+  dplyr::select(site_id,site_lon_trapnest,site_lat_trapnest) |> 
   mutate(site_id = as.numeric(site_id),
          site_lon_trapnest = as.numeric(site_lon_trapnest),
          site_lat_trapnest = as.numeric(site_lat_trapnest))
