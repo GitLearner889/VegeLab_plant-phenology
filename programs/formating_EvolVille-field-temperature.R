ea15fbad0857f0de4e182694e0383b8bcff35438
Gabriel Sagot <gabriel.sagot@proton.me>
2026-09-16 14:29:18 +0200

    clean temperature data: standardization of date and input of the time of day (day time or night time)

--- programs/formating_EvolVille-field-temperature.R
+++ programs/formating_EvolVille-field-temperature.R
@@ -20,5 +20,20 @@ df_sites_positions = df_sites_info |>
          site_lon_trapnest = as.numeric(site_lon_trapnest),
          site_lat_trapnest = as.numeric(site_lat_trapnest))
 
+##### 1.2 iButton data #####
+# format date and time of temperature data
+df_temp = df_temperature_all_raw %>% 
+  dplyr::select(date,time,site,temp,hum,dewpoint,day_night) |> # keep only intresting variables
+  rename(site_id = site) |> 
+  mutate(datetime = as.POSIXct( # create a global variable of date and time, of date class
+    paste(as.Date(date,format = "%Y-%m-%d"), time), 
+    format = "%Y-%m-%d %H:%M:%S"),
+    tz = "Europe/Paris") 
+# add day or night information 
+df_temp = df_temp %>% 
+  mutate(day_time = is_daytime(date = datetime,
+                               tz = "Europe/Paris", 
+                               geocode = tibble(lon = 7.76811, lat = 48.58325))) %>% # choose the location of botanical garden of Strasbourg (to heavy to implement it for each specific location, and the results is almost the same)
+  mutate(day_time = ifelse(day_time, "Day", "Night"))
 
 
