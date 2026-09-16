f3bab0b467bfc76441ca074b10b1c2c3e0439469
Gabriel Sagot <gabriel.sagot@proton.me>
2026-09-16 14:20:06 +0200

    data retrieval (temperature & site location)

--- programs/formating_EvolVille-field-temperature.R
+++ programs/formating_EvolVille-field-temperature.R
@@ -3,3 +3,10 @@ source(here::here("programs","libraries.R"))
 requires(dplyr)
 requires(photobiology)
 
+#### 0. Data importation ####
+
+# iButton data retrival 
+df_temperature_all_raw = read.table(here("data/data_environnement/ibutton_all.txt"), header = T, sep = "\t")
+# site location data retrival
+df_sites_info = read_excel(here("data/site.xlsx"))
+
