f75a7faa329bf8e7cf650032c72d8f186e4a8667
Gabriel Sagot <gabriel.sagot@proton.me>
2026-09-22 18:29:48 +0200

    small corrections

--- functions/funct_spatial-iterpolation.R
+++ functions/funct_spatial-iterpolation.R
@@ -1,14 +1,15 @@
+# Function original from Alejandro Sotillo
 
 ### Inverse Distance Weighting
 ## ROLE: 
-#THis function estimates values for unknown spatial location from known data of determined locations and values by using the principle of the closer point are the more similare the values should be. The exact formula : the sum of values of all points weighted by the distance of the points to the point of interpolation, divided by the sum of the distance bteween all the points and the interpolation point.
+#This function estimates values for unknown spatial location from known data of determined locations and values by using the principle of the closer point are the more similar the values should be. The exact formula : the sum of values of all points weighted by the distance of the points to the point of interpolation, divided by the sum of the distance between all the points and the interpolation point.
 # All about it on: https://en.wikipedia.org/wiki/Inverse_distance_weighting
 
 ## ARGUMENTS:
-# empty_positions: dataframe of position of all unknown points: id, location
-# data_positions: dataframe of position of all known points: id, location
-# data_values: dataframe of values of all known points: id, value (e.g. temperature) 
-# beta: power parameter that give the speed at which weigth decline with distance (great beta = only close sites matters) ; usually between 1 and 4
+# empty_positions: data-frame of position of all unknown points: id, location
+# data_positions: data-frame of position of all known points: id, location
+# data_values: data-frame of values of all known points: id, value (e.g. temperature) 
+# beta: power parameter that give the speed at which weight decline with distance (great beta = only close sites matters) ; usually between 1 and 4
 # radius: distance above which the points should not be considered to calculate the unknown value 
 
 
@@ -16,9 +17,9 @@
 
 idw_point <- function(empty_positions, data_positions, data_values, beta, radius = NULL){
   
-  require(data.table)
-  require(sf)
-  require(DescTools)
+  # require(data.table)
+  # require(sf)
+  # require(DescTools)
   
   empty_positions_sf <- st_as_sf(empty_positions, coords = names(empty_positions),remove=F) %>% 
     st_set_crs(4326)
@@ -38,6 +39,5 @@ idw_point <- function(empty_positions, data_positions, data_values, beta, radius
   calctable[, output := numerator/denominator]
   
   return(calctable$output)
-  
 }
 
